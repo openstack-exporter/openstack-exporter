87b2a43a01f36a5b6b588ffb69e0bacfd113344c
Jorge Niedbalski <niedbalski@gmail.com>
2019-12-23 18:51:20 -0300

    Implement object-store metrics (#76)
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- exporters/exporter.go
+++ exporters/exporter.go
@@ -198,6 +198,13 @@ func NewExporter(name, prefix, cloud string, disabledMetrics []string, endpointT
 				return nil, err
 			}
 		}
+	case "object-store":
+		{
+			exporter, err = NewObjectStoreExporter(client, prefix, disabledMetrics)
+			if err != nil {
+				return nil, err
+			}
+		}
 	default:
 		{
 			return nil, fmt.Errorf("couldn't find a handler for %s exporter", name)

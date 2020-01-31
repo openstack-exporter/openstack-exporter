297610956c09b2e1716e232c6caee4267a59ef5b
Tristan Zhang <ztianzhi@gmail.com>
2020-01-30 22:19:37 -0500

    Add metrics total_amphorae and amphora_status to loadbalancer exporter.

--- exporters/loadbalancer.go
+++ exporters/loadbalancer.go
@@ -2,6 +2,7 @@ package exporters
 
 import (
 	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack/loadbalancer/v2/amphorae"
 	"github.com/gophercloud/gophercloud/openstack/loadbalancer/v2/loadbalancers"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -17,8 +18,28 @@ var loadbalancer_status = []string{
 	"NO_MONITOR", // No health monitor is configured for this entity and it's status is unknown
 }
 
+var amphora_status = []string{
+	// The status of the amphora. One of: BOOTING, ALLOCATED, READY, PENDING_CREATE, PENDING_DELETE, DELETED, ERROR.
+	"BOOTING",
+	"ALLOCATED",
+	"READY",
+	"PENDING_CREATE",
+	"PENDING_DELETE",
+	"DELETED",
+	"ERROR",
+}
+
 func mapLoadbalancerStatus(current string) int {
 	for idx, status := range loadbalancer_status {
+		if current == status {
+			return idx
+		}
+	}
+	return -1
+}
+
+func mapAmphoraStatus(current string) int {
+	for idx, status := range amphora_status {
 		if current == status {
 			return idx
 		}
@@ -33,6 +54,8 @@ type LoadbalancerExporter struct {
 var defaultLoadbalancerMetrics = []Metric{
 	{Name: "total_loadbalancers", Fn: ListAllLoadbalancers},
 	{Name: "loadbalancer_status", Labels: []string{"id", "name", "project_id", "operating_status", "provisioning_status", "provider", "vip_address"}},
+	{Name: "total_amphorae", Fn: ListAllAmphorae},
+	{Name: "amphora_status", Labels: []string{"id", "loadbalancer_id", "compute_id", "status", "role", "lb_network_ip", "ha_ip"}},
 }
 
 func NewLoadbalancerExporter(client *gophercloud.ServiceClient, prefix string, disabledMetrics []string) (*LoadbalancerExporter, error) {
@@ -67,6 +90,27 @@ func ListAllLoadbalancers(exporter *BaseOpenStackExporter, ch chan<- prometheus.
 		ch <- prometheus.MustNewConstMetric(exporter.Metrics["loadbalancer_status"].Metric,
 			prometheus.GaugeValue, float64(mapLoadbalancerStatus(loadbalancer.OperatingStatus)), loadbalancer.ID, loadbalancer.Name, loadbalancer.ProjectID,
 			loadbalancer.OperatingStatus, loadbalancer.ProvisioningStatus, loadbalancer.Provider, loadbalancer.VipAddress)
+	}
+	return nil
+}
+
+func ListAllAmphorae(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) error {
+	var allAmphorae []amphorae.Amphora
+	allPagesAmphorae, err := amphorae.List(exporter.Client, amphorae.ListOpts{}).AllPages()
+	if err != nil {
+		return err
+	}
+	allAmphorae, err = amphorae.ExtractAmphorae(allPagesAmphorae)
+	if err != nil {
+		return err
+	}
+	ch <- prometheus.MustNewConstMetric(exporter.Metrics["total_amphorae"].Metric,
+		prometheus.GaugeValue, float64(len(allAmphorae)))
+	// Loadbalancer status metrics
+	for _, amphora := range allAmphorae {
+		ch <- prometheus.MustNewConstMetric(exporter.Metrics["amphora_status"].Metric,
+			prometheus.GaugeValue, float64(mapAmphoraStatus(amphora.Status)), amphora.ID, amphora.LoadbalancerID, amphora.ComputeID, amphora.Status,
+			amphora.Role, amphora.LBNetworkIP, amphora.HAIP)
 	}
 	return nil
 }

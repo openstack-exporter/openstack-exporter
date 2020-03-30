cb5d9f3c4d591c8c916f32b67b54d76ea3e7a6da
Tristan Zhang <ztianzhi@gmail.com>
2020-03-30 12:06:14 -0400

    Add file containerinfra_test.go

---
+++ exporters/containerinfra_test.go
@@ -0,0 +1,43 @@
+package exporters
+
+import (
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+type ContainerInfraTestSuite struct {
+	BaseOpenStackTestSuite
+}
+
+var containerInfraExpectedUp = `
+# HELP openstack_container_infra_cluster_status cluster_status
+# TYPE openstack_container_infra_cluster_status gauge
+openstack_container_infra_cluster_status{master_count="1",name="k8s",node_count="1",stack_id="31c1ee6c-081e-4f39-9f0f-f1d87a7defa1",status="CREATE_FAILED",uuid="273c39d5-fa17-4372-b6b1-93a572de2cef"} 1
+# HELP openstack_container_infra_total_clusters total_clusters
+# TYPE openstack_container_infra_total_clusters gauge
+openstack_container_infra_total_clusters 1
+# HELP openstack_container_infra_up up
+# TYPE openstack_container_infra_up gauge
+openstack_container_infra_up 1
+`
+
+var containerInfraExpectedDown = `
+# HELP openstack_container_infra_up up
+# TYPE openstack_container_infra_up gauge
+openstack_container_infra_up 0
+`
+
+func (suite *ContainerInfraTestSuite) TestContainerInfraExporter() {
+	err := testutil.CollectAndCompare(*suite.Exporter, strings.NewReader(containerInfraExpectedUp))
+	assert.NoError(suite.T(), err)
+}
+
+func (suite *ContainerInfraTestSuite) TestContainerInfraExporterWithEndpointDown() {
+	suite.teardownFixtures()
+	defer suite.installFixtures()
+
+	err := testutil.CollectAndCompare(*suite.Exporter, strings.NewReader(containerInfraExpectedDown))
+	assert.NoError(suite.T(), err)
+}

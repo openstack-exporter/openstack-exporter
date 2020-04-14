4e8f6b6e910133d487f267cad4d6a74a3f20e2ad
Mohammed Naser <mnaser@vexxhost.com>
2020-04-14 16:31:59 -0400

    Performance improvements + port status addition (#97)
    * Add openstack_neutron_port
    
    Instead of presenting aggregates of data, we should expose all the data
    that we have and let our users instead using things like recording rules
    to aggregate them.
    
    This patch also refactors the Neutron bits to not make multiple requests
    to all ports which puts a huge strain on the cluster.
    
    * Fail tests if we call an endpoint more than once
    
    The neutron exporter seems to have a lot of "active/inactive" references
    which are doing a query every single time, resulting in duplicate API
    calls on very heavy operations (aka list all routers/ports in a cloud).
    
    This patch updates testing to allow calling a URL only *once* during
    tests which should eliminate these from making their way into the
    code.

--- exporters/neutron.go
+++ exporters/neutron.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/layer3/routers"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/lbaas_v2/loadbalancers"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/networkipavailabilities"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/portsbinding"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/security/groups"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/networks"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/ports"
@@ -23,20 +24,21 @@ type NeutronExporter struct {
 
 var defaultNeutronMetrics = []Metric{
 	{Name: "floating_ips", Fn: ListFloatingIps},
-	{Name: "floating_ips_associated_not_active", Fn: ListFloatingIpsAssociatedNotActive},
+	{Name: "floating_ips_associated_not_active"},
 	{Name: "networks", Fn: ListNetworks},
 	{Name: "security_groups", Fn: ListSecGroups},
 	{Name: "subnets", Fn: ListSubnets},
-	{Name: "ports", Fn: ListPorts},
-	{Name: "ports_no_ips", Fn: ListPortsNoIPs},
-	{Name: "ports_lb_not_active", Fn: ListPortsLBsNotActive},
+	{Name: "port", Labels: []string{"uuid", "network_id", "mac_address", "device_owner", "status", "binding_vif_type"}, Fn: ListPorts},
+	{Name: "ports"},
+	{Name: "ports_no_ips"},
+	{Name: "ports_lb_not_active"},
 	{Name: "routers", Fn: ListRouters},
-	{Name: "routers_not_active", Fn: ListRoutersNotActive},
+	{Name: "routers_not_active"},
 	{Name: "agent_state", Labels: []string{"hostname", "service", "adminState"}, Fn: ListAgentStates},
 	{Name: "network_ip_availabilities_total", Labels: []string{"network_id", "network_name", "ip_version", "cidr", "subnet_name", "project_id"}, Fn: ListNetworkIPAvailabilities},
 	{Name: "network_ip_availabilities_used", Labels: []string{"network_id", "network_name", "ip_version", "cidr", "subnet_name", "project_id"}},
 	{Name: "loadbalancers", Fn: ListLBs},
-	{Name: "loadbalancers_not_active", Fn: ListLBsNotActive},
+	{Name: "loadbalancers_not_active"},
 }
 
 // NewNeutronExporter : returns a pointer to NeutronExporter
@@ -57,27 +59,8 @@ func NewNeutronExporter(client *gophercloud.ServiceClient, prefix string, disabl
 	return &exporter, nil
 }
 
-// ListFloatingIps : count total number of instantiated FloatingIPs
+// ListFloatingIps : count total number of instantiated FloatingIPs and those that are associated to private IP but not in ACTIVE state
 func ListFloatingIps(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) error {
-	var allFloatingIPs []floatingips.FloatingIP
-
-	allPagesFloatingIPs, err := floatingips.List(exporter.Client, floatingips.ListOpts{}).AllPages()
-	if err != nil {
-		return err
-	}
-
-	allFloatingIPs, err = floatingips.ExtractFloatingIPs(allPagesFloatingIPs)
-	if err != nil {
-		return err
-	}
-	ch <- prometheus.MustNewConstMetric(exporter.Metrics["floating_ips"].Metric,
-		prometheus.GaugeValue, float64(len(allFloatingIPs)))
-
-	return nil
-}
-
-// ListFloatingIpsAssociatedNotActive : count total number of instantiated FloatingIPs that are associated to private IP but not in ACTIVE state
-func ListFloatingIpsAssociatedNotActive(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) error {
 	var allFloatingIPs []floatingips.FloatingIP
 
 	allPagesFloatingIPs, err := floatingips.List(exporter.Client, floatingips.ListOpts{}).AllPages()
@@ -99,6 +82,8 @@ func ListFloatingIpsAssociatedNotActive(exporter *BaseOpenStackExporter, ch chan
 		}
 	}
 
+	ch <- prometheus.MustNewConstMetric(exporter.Metrics["floating_ips"].Metric,
+		prometheus.GaugeValue, float64(len(allFloatingIPs)))
 	ch <- prometheus.MustNewConstMetric(exporter.Metrics["floating_ips_associated_not_active"].Metric,
 		prometheus.GaugeValue, float64(failedFIPs))
 
@@ -193,78 +178,54 @@ func ListSubnets(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) e
 	return nil
 }
 
-// ListPorts : count total number of instantiated Ports
+// PortBinding represents a port which includes port bindings
+type PortBinding struct {
+	ports.Port
+	portsbinding.PortsBindingExt
+}
+
+// ListPorts generates metrics about ports inside the OpenStack cloud
 func ListPorts(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) error {
-	var allPorts []ports.Port
+	var allPorts []PortBinding
 
 	allPagesPorts, err := ports.List(exporter.Client, ports.ListOpts{}).AllPages()
 	if err != nil {
 		return err
 	}
 
-	allPorts, err = ports.ExtractPorts(allPagesPorts)
+	err = ports.ExtractPortsInto(allPagesPorts, &allPorts)
 	if err != nil {
 		return err
 	}
 
+	portsWithNoIP := float64(0)
+	lbaasPortsInactive := float64(0)
+
+	for _, port := range allPorts {
+		if port.Status == "ACTIVE" && len(port.FixedIPs) == 0 {
+			portsWithNoIP++
+		}
+
+		if port.DeviceOwner == "neutron:LOADBALANCERV2" && port.Status != "ACTIVE" {
+			lbaasPortsInactive++
+		}
+
+		ch <- prometheus.MustNewConstMetric(exporter.Metrics["port"].Metric,
+			prometheus.GaugeValue, 1, port.ID, port.NetworkID, port.MACAddress, port.DeviceOwner, port.Status, port.VIFType)
+	}
+
+	// NOTE(mnaser): We should deprecate this and users can replace it by
+	//               count(openstack_neutron_port)
 	ch <- prometheus.MustNewConstMetric(exporter.Metrics["ports"].Metric,
 		prometheus.GaugeValue, float64(len(allPorts)))
 
-	return nil
-}
-
-// ListPortsNoIPs : count total number of ACTIVE Ports with no IP
-func ListPortsNoIPs(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) error {
-	var allPorts []ports.Port
-	var opts = ports.ListOpts{Status: "ACTIVE"}
-
-	allPagesPorts, err := ports.List(exporter.Client, opts).AllPages()
-	if err != nil {
-		return err
-	}
-
-	allPorts, err = ports.ExtractPorts(allPagesPorts)
-	if err != nil {
-		return err
-	}
-
-	failedPorts := 0
-	for _, port := range allPorts {
-		if len(port.FixedIPs) == 0 {
-			failedPorts = failedPorts + 1
-		}
-	}
+	// NOTE(mnaser): We should deprecate this and users can replace it by:
+	//               count(openstack_neutron_port{device_owner="neutron:LOADBALANCERV2",status!="ACTIVE"})
+	ch <- prometheus.MustNewConstMetric(exporter.Metrics["ports_lb_not_active"].Metric,
+		prometheus.GaugeValue, lbaasPortsInactive)
 
 	ch <- prometheus.MustNewConstMetric(exporter.Metrics["ports_no_ips"].Metric,
-		prometheus.GaugeValue, float64(failedPorts))
-
-	return nil
-}
-
-// ListPortsLBsNotActive : count total number of LB Ports that are not in ACTIVE state
-func ListPortsLBsNotActive(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) error {
-	var allPorts []ports.Port
-	var opts = ports.ListOpts{DeviceOwner: "neutron:LOADBALANCERV2"}
-
-	allPagesPorts, err := ports.List(exporter.Client, opts).AllPages()
-	if err != nil {
-		return err
-	}
-
-	allPorts, err = ports.ExtractPorts(allPagesPorts)
-	if err != nil {
-		return err
-	}
-
-	failedPorts := 0
-	for _, port := range allPorts {
-		if port.Status != "ACTIVE" {
-			failedPorts = failedPorts + 1
-		}
-	}
-
-	ch <- prometheus.MustNewConstMetric(exporter.Metrics["ports_lb_not_active"].Metric,
-		prometheus.GaugeValue, float64(failedPorts))
+		prometheus.GaugeValue, portsWithNoIP)
 
 	return nil
 }
@@ -308,28 +269,8 @@ func ListNetworkIPAvailabilities(exporter *BaseOpenStackExporter, ch chan<- prom
 	return nil
 }
 
-// ListRouters : count total number of instantiated Routers
+// ListRouters : count total number of instantiated Routers and those that are not in ACTIVE state
 func ListRouters(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) error {
-	var allRouters []routers.Router
-
-	allPagesRouters, err := routers.List(exporter.Client, routers.ListOpts{}).AllPages()
-	if err != nil {
-		return err
-	}
-
-	allRouters, err = routers.ExtractRouters(allPagesRouters)
-	if err != nil {
-		return err
-	}
-
-	ch <- prometheus.MustNewConstMetric(exporter.Metrics["routers"].Metric,
-		prometheus.GaugeValue, float64(len(allRouters)))
-
-	return nil
-}
-
-// ListRoutersNotActive : count total number of instantiated Routers that are not in ACTIVE state
-func ListRoutersNotActive(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) error {
 	var allRouters []routers.Router
 
 	allPagesRouters, err := routers.List(exporter.Client, routers.ListOpts{}).AllPages()
@@ -349,34 +290,16 @@ func ListRoutersNotActive(exporter *BaseOpenStackExporter, ch chan<- prometheus.
 		}
 	}
 
+	ch <- prometheus.MustNewConstMetric(exporter.Metrics["routers"].Metric,
+		prometheus.GaugeValue, float64(len(allRouters)))
 	ch <- prometheus.MustNewConstMetric(exporter.Metrics["routers_not_active"].Metric,
 		prometheus.GaugeValue, float64(failedRouters))
 
 	return nil
 }
 
-// ListLBs : count total number of instantiated LoadBalancers
+// ListLBs : count total number of instantiated LoadBalancers and those that are not in ACTIVE state
 func ListLBs(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) error {
-	var allLBs []loadbalancers.LoadBalancer
-
-	allPagesLBs, err := loadbalancers.List(exporter.Client, loadbalancers.ListOpts{}).AllPages()
-	if err != nil {
-		return err
-	}
-
-	allLBs, err = loadbalancers.ExtractLoadBalancers(allPagesLBs)
-	if err != nil {
-		return err
-	}
-
-	ch <- prometheus.MustNewConstMetric(exporter.Metrics["loadbalancers"].Metric,
-		prometheus.GaugeValue, float64(len(allLBs)))
-
-	return nil
-}
-
-// ListLBsNotActive : count total number of instantiated LoadBalancers that are not in ACTIVE state
-func ListLBsNotActive(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metric) error {
 	var allLBs []loadbalancers.LoadBalancer
 
 	allPagesLBs, err := loadbalancers.List(exporter.Client, loadbalancers.ListOpts{}).AllPages()
@@ -396,6 +319,8 @@ func ListLBsNotActive(exporter *BaseOpenStackExporter, ch chan<- prometheus.Metr
 		}
 	}
 
+	ch <- prometheus.MustNewConstMetric(exporter.Metrics["loadbalancers"].Metric,
+		prometheus.GaugeValue, float64(len(allLBs)))
 	ch <- prometheus.MustNewConstMetric(exporter.Metrics["loadbalancers_not_active"].Metric,
 		prometheus.GaugeValue, float64(failedLBs))
 

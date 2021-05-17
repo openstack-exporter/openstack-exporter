391ea1e6a59a920ccda39c6aba96be6796449975
skunkie <skunkie@users.noreply.github.com>
2021-05-17 15:48:02 +0300

    Implemented database exporter with 3 new metrics
    openstack_trove_instance_status
    openstack_trove_instance_volume_size_gb
    openstack_trove_instance_volume_used_gb

--- exporters/exporter.go
+++ exporters/exporter.go
@@ -299,6 +299,13 @@ func NewExporter(name, prefix, cloud string, disabledMetrics []string, endpointT
 				return nil, err
 			}
 		}
+	case "database":
+		{
+			exporter, err = NewTroveExporter(&exporterConfig)
+			if err != nil {
+				return nil, err
+			}
+		}
 	default:
 		{
 			return nil, fmt.Errorf("couldn't find a handler for %s exporter", name)

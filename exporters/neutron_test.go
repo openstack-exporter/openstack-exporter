4c9f9002671270baba79f51910280b9cd140afe4
Vladimir Lobanov <v.lobanov@it.ecommpay.com>
2023-09-04 15:23:23 +0300

    Add new neutron metrics
    neutron_network and neutron_subnet metrics was added.

--- exporters/neutron_test.go
+++ exporters/neutron_test.go
@@ -35,6 +35,10 @@ openstack_neutron_floating_ips_associated_not_active 1
 # TYPE openstack_neutron_l3_agent_of_router gauge
 openstack_neutron_l3_agent_of_router{agent_admin_up="true",agent_alive="true",agent_host="dev-os-ctrl-02",ha_state="",l3_agent_id="ddbf087c-e38f-4a73-bcb3-c38f2a719a03",router_id="9daeb7dd-7e3f-4e44-8c42-c7a0e8c8a42f"} 1
 openstack_neutron_l3_agent_of_router{agent_admin_up="true",agent_alive="true",agent_host="dev-os-ctrl-02",ha_state="",l3_agent_id="ddbf087c-e38f-4a73-bcb3-c38f2a719a03",router_id="f8a44de0-fc8e-45df-93c7-f79bf3b01c95"} 1
+# HELP openstack_neutron_network network
+# TYPE openstack_neutron_network gauge
+openstack_neutron_network{id="d32019d3-bc6e-4319-9c1d-6722fc136a22",is_external="false",is_shared="false",name="net1",provider_network_type="vlan",provider_physical_network="public",provider_segmentation_id="3",status="ACTIVE",subnets="54d6f61d-db07-451c-9ab3-b9609b6b6f0b",tags="tag1,tag2",tenant_id="4fd44f30292945e481c7b8a0c8908869"} 0
+openstack_neutron_network{id="db193ab3-96e3-4cb3-8fc5-05f4296d0324",is_external="false",is_shared="false",name="net2",provider_network_type="local",provider_physical_network="",provider_segmentation_id="",status="ACTIVE",subnets="08eae331-0402-425a-923c-34f7cfe39c1b",tags="tag1,tag2",tenant_id="26a7980765d0414dbc1fc1f88cdb7e6e"} 0
 # HELP openstack_neutron_network_ip_availabilities_total network_ip_availabilities_total
 # TYPE openstack_neutron_network_ip_availabilities_total gauge
 openstack_neutron_network_ip_availabilities_total{cidr="10.0.0.0/24",ip_version="4",network_id="6801d9c8-20e6-4b27-945d-62499f00002e",network_name="private",project_id="d56d3b8dd6894a508cf41b96b522328c",subnet_name="private-subnet"} 253
@@ -49,7 +53,7 @@ openstack_neutron_network_ip_availabilities_used{cidr="2001:db8::/64",ip_version
 openstack_neutron_network_ip_availabilities_used{cidr="fdbf:ac66:9be8::/64",ip_version="6",network_id="6801d9c8-20e6-4b27-945d-62499f00002e",network_name="private",project_id="d56d3b8dd6894a508cf41b96b522328c",subnet_name="ipv6-private-subnet"} 2
 # HELP openstack_neutron_networks networks
 # TYPE openstack_neutron_networks gauge
-openstack_neutron_networks 0
+openstack_neutron_networks 2
 # HELP openstack_neutron_port port
 # TYPE openstack_neutron_port gauge
 openstack_neutron_port{admin_state_up="true",binding_vif_type="",device_owner="network:router_gateway",mac_address="fa:16:3e:58:42:ed",network_id="70c1db1f-b701-45bd-96e0-a313ee3430b3",status="ACTIVE",uuid="d80b1a3b-4fc1-49f3-952e-1e2ab7081d8b"} 1
@@ -77,6 +81,12 @@ openstack_neutron_routers_not_active 1
 # HELP openstack_neutron_security_groups security_groups
 # TYPE openstack_neutron_security_groups gauge
 openstack_neutron_security_groups 1
+# HELP openstack_neutron_subnet subnet
+# TYPE openstack_neutron_subnet gauge
+openstack_neutron_subnet{cidr="10.0.0.0/24",dns_nameservers="",enable_dhcp="true",gateway_ip="10.0.0.1",id="08eae331-0402-425a-923c-34f7cfe39c1b",name="private-subnet",network_id="db193ab3-96e3-4cb3-8fc5-05f4296d0324",tags="tag1,tag2",tenant_id="26a7980765d0414dbc1fc1f88cdb7e6e"} 1
+openstack_neutron_subnet{cidr="10.10.0.0/24",dns_nameservers="",enable_dhcp="true",gateway_ip="10.10.0.1",id="12769bb8-6c3c-11ec-8124-002b67875abf",name="pooled-subnet-ipv4",network_id="d32019d3-bc6e-4319-9c1d-6722fc136a22",tags="tag1,tag2",tenant_id="4fd44f30292945e481c7b8a0c8908869"} 1
+openstack_neutron_subnet{cidr="192.0.0.0/8",dns_nameservers="",enable_dhcp="true",gateway_ip="192.0.0.1",id="54d6f61d-db07-451c-9ab3-b9609b6b6f0b",name="my_subnet",network_id="d32019d3-bc6e-4319-9c1d-6722fc136a22",tags="tag1,tag2",tenant_id="4fd44f30292945e481c7b8a0c8908869"} 1
+openstack_neutron_subnet{cidr="2001:db8::/64",dns_nameservers="",enable_dhcp="true",gateway_ip="2001:db8::1",id="f73defec-6c43-11ec-a08b-002b67875abf",name="pooled-subnet-ipv6",network_id="d32019d3-bc6e-4319-9c1d-6722fc136a22",tags="tag1,tag2",tenant_id="4fd44f30292945e481c7b8a0c8908869"} 1
 # HELP openstack_neutron_subnets subnets
 # TYPE openstack_neutron_subnets gauge
 openstack_neutron_subnets 4

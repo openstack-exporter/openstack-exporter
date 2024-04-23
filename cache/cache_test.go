71169778e9224aa30bc2dd2c7e4ab84b21613824
jneo8 <james.lin@canonical.com>
2024-04-23 15:45:10 +0800

    refactor: Change the cache backend interface implementation
    - Remove un-used get/set functions in the interface.
    - Refactor the cache data structure, now is only two layers.

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -4,24 +4,19 @@ import (
 	"testing"
 
 	"time"
-
-	dto "github.com/prometheus/client_model/go"
 )
 
 // TestSetAndGetCloudCache tests setting and getting cloud cache data.
 func TestInMemoryCachSetAndGetCloudCache(t *testing.T) {
 	cache := GetCache()
-	defer cache.Clear()
+	defer cache.FlushExpiredCloudCaches(1 * time.Nanosecond)
 	cloudName := "testCloud"
 
 	_, exists := cache.GetCloudCache(cloudName)
 	if exists {
 		t.Errorf("Cloud cache not retrieved properly")
 	}
-	cloudData := CloudCache{
-		Time:          time.Now(),
-		ServiceCaches: make(map[string]*ServiceCache),
-	}
+	cloudData := NewCloudCache()
 
 	cache.SetCloudCache(cloudName, cloudData)
 	retrievedCloudData, exists := cache.GetCloudCache(cloudName)
@@ -31,62 +26,11 @@ func TestInMemoryCachSetAndGetCloudCache(t *testing.T) {
 	}
 }
 
-// TestSetAndGetServiceCache tests setting and getting service cache data.
-func TestInMemoryCachSetAndGetServiceCache(t *testing.T) {
-	cache := GetCache()
-	defer cache.Clear()
-	cloudName := "testCloud"
-	serviceName := "testService"
-
-	_, exists := cache.GetServiceCache(cloudName, serviceName)
-	if exists {
-		t.Errorf("Cloud cache not retrieved properly")
-	}
-
-	serviceData := ServiceCache{
-		MetricFamilyCaches: make(map[string]*MetricFamilyCache),
-	}
-
-	cache.SetServiceCache(cloudName, serviceName, serviceData)
-	retrievedServiceData, exists := cache.GetServiceCache(cloudName, serviceName)
-
-	if !exists || retrievedServiceData.Time.IsZero() {
-		t.Errorf("Service cache was not set or retrieved properly")
-	}
-}
-
-// TestSetAndGetMetricFamilyCache tests setting and getting metric family cache data.
-func TestInMemoryCachSetAndGetMetricFamilyCache(t *testing.T) {
-	cache := GetCache()
-	defer cache.Clear()
-	cloudName := "testCloud"
-	serviceName := "testService"
-	mfName := "testMetricFamily"
-
-	_, exists := cache.GetMetricFamilyCache(cloudName, serviceName, mfName)
-	if exists {
-		t.Errorf("Cloud cache not retrieved properly")
-	}
-
-	mfData := MetricFamilyCache{
-		MF: &dto.MetricFamily{},
-	}
-
-	cache.SetMetricFamilyCache(cloudName, serviceName, mfName, mfData)
-	retrievedMfData, exists := cache.GetMetricFamilyCache(cloudName, serviceName, mfName)
-
-	if !exists || retrievedMfData.Time.IsZero() {
-		t.Errorf("Metric family cache was not set or retrieved properly")
-	}
-}
-
 // TestFlushExpiredCloudCaches tests flushing of expired cloud caches.
 func TestInMemoryCacheFlushExpiredCloudCaches(t *testing.T) {
 	cache := GetCache()
-	defer cache.Clear()
-	cloudData := CloudCache{
-		ServiceCaches: make(map[string]*ServiceCache),
-	}
+	defer cache.FlushExpiredCloudCaches(1 * time.Nanosecond)
+	cloudData := NewCloudCache()
 	cloudName := "expiredCloud"
 	cache.SetCloudCache(cloudName, cloudData)
 
@@ -100,14 +44,41 @@ func TestInMemoryCacheFlushExpiredCloudCaches(t *testing.T) {
 
 func TestInMemoryCachInit(t *testing.T) {
 	cache := InMemoryCache{}
-	defer cache.Clear()
+	defer cache.FlushExpiredCloudCaches(1 * time.Nanosecond)
 	cloudName := "testCloud"
-	serviceName := "testService"
-	mfName := "testMetricFamily"
 
-	cache.init(&cloudName, &serviceName, &mfName)
-	_, exists := cache.GetMetricFamilyCache(cloudName, serviceName, mfName)
+	cache.init(&cloudName)
+	_, exists := cache.GetCloudCache(cloudName)
 	if !exists {
 		t.Errorf("Init function not works properly")
+	}
+}
+
+func TestNewCloudCache(t *testing.T) {
+	cloudCache := NewCloudCache()
+	if cloudCache.Time.IsZero() {
+		t.Errorf("Time doesn't been setup properly")
+	}
+	if cloudCache.MetricFamilyCaches == nil {
+		t.Errorf("MetricFamilyCaches doesn't been setup properly")
+	}
+}
+
+func TestCloudCacheSetMetricFamilyCache(t *testing.T) {
+	cache := GetCache()
+	defer cache.FlushExpiredCloudCaches(1 * time.Nanosecond)
+	cloudCache := NewCloudCache()
+
+	serviceName := "testService"
+	mfName := "testMF"
+	cloudCache.SetMetricFamilyCache(
+		mfName, MetricFamilyCache{MF: nil, Service: serviceName},
+	)
+
+	if cloudCache.Time.IsZero() {
+		t.Errorf("Time doesn't been setup properly")
+	}
+	if l := len(cloudCache.MetricFamilyCaches); l != 1 {
+		t.Errorf("SetMetricFamilyCache not set value properly")
 	}
 }

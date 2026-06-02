39c78c585cb6eff83e4fd045d0b119fcfe77faa1
Sharpz7 <adam.mcarthur62@gmail.com>
2026-06-02 19:02:15 +0000

    feat: add ironic node labels

--- exporters/ironic.go
+++ exporters/ironic.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log/slog"
 	"strconv"
+	"strings"
 
 	"github.com/gophercloud/gophercloud/v2/openstack/baremetal/v1/nodes"
 	"github.com/openstack-exporter/openstack-exporter/utils"
@@ -18,7 +19,7 @@ type IronicExporter struct {
 }
 
 var defaultIronicMetrics = []Metric{
-	{Name: "node", Labels: []string{"id", "name", "provision_state", "power_state", "maintenance", "maintenance_reason", "console_enabled", "resource_class", "deploy_kernel", "deploy_ramdisk", "retired", "retired_reason"}, Fn: ListNodes},
+	{Name: "node", Labels: []string{"id", "name", "provision_state", "power_state", "maintenance", "maintenance_reason", "conductor_group", "traits", "instance_uuid", "last_error", "serial_number", "console_enabled", "resource_class", "deploy_kernel", "deploy_ramdisk", "retired", "retired_reason", "ironic_self_healing_state"}, Fn: ListNodes},
 	{Name: "node_updated_at", Labels: []string{"id", "name", "provision_state"}, Fn: nil},
 	{Name: "node_provision_updated_at", Labels: []string{"id", "name", "provision_state"}, Fn: nil},
 }
@@ -72,11 +73,14 @@ func ListNodes(ctx context.Context, exporter *BaseOpenStackExporter, ch chan<- p
 	for _, node := range allNodes {
 		deployKernel := getDriverInfoString(node.DriverInfo, "deploy_kernel")
 		deployRamdisk := getDriverInfoString(node.DriverInfo, "deploy_ramdisk")
+		serialNumber := getNestedExtraString(node.Extra, "system_vendor", "serial_number")
+		ironicSelfHealingState := getExtraString(node.Extra, "ironic_self_healing_state")
 
 		ch <- prometheus.MustNewConstMetric(exporter.Metrics["node"].Metric,
 			prometheus.GaugeValue, 1.0, node.UUID, node.Name, node.ProvisionState, node.PowerState,
-			strconv.FormatBool(node.Maintenance), node.MaintenanceReason, strconv.FormatBool(node.ConsoleEnabled), node.ResourceClass,
-			deployKernel, deployRamdisk, strconv.FormatBool(node.Retired), node.RetiredReason)
+			strconv.FormatBool(node.Maintenance), sanitizeMetricString(node.MaintenanceReason), node.ConductorGroup, strings.Join(node.Traits, " "),
+			node.InstanceUUID, sanitizeMetricString(node.LastError), serialNumber, strconv.FormatBool(node.ConsoleEnabled), node.ResourceClass,
+			deployKernel, deployRamdisk, strconv.FormatBool(node.Retired), node.RetiredReason, ironicSelfHealingState)
 
 		if !node.UpdatedAt.IsZero() {
 			ch <- prometheus.MustNewConstMetric(
@@ -102,6 +106,41 @@ func ListNodes(ctx context.Context, exporter *BaseOpenStackExporter, ch chan<- p
 	}
 
 	return nil
+}
+
+func getExtraString(extra map[string]any, key string) string {
+	if extra == nil {
+		return ""
+	}
+
+	value, ok := extra[key]
+	if !ok {
+		return ""
+	}
+
+	s, ok := value.(string)
+	if !ok {
+		return ""
+	}
+
+	return s
+}
+
+func getNestedExtraString(extra map[string]any, key string, nestedKey string) string {
+	if extra == nil {
+		return ""
+	}
+
+	nested, ok := extra[key].(map[string]any)
+	if !ok {
+		return ""
+	}
+
+	return getExtraString(nested, nestedKey)
+}
+
+func sanitizeMetricString(value string) string {
+	return strings.TrimSpace(strings.ReplaceAll(value, "\n", " "))
 }
 
 func getDriverInfoString(driverInfo map[string]any, key string) string {
